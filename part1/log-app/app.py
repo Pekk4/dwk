d8f8ba07574f9d705147713fe2b6226340deb4bc
Pekka <75335748+Pekk4@users.noreply.github.com>
2022-06-26 18:00:57 +0300

    Exercise 1.07

--- part1/log-app/app.py
+++ part1/log-app/app.py
@@ -1,10 +1,27 @@
 import secrets
 import time
 import sys
+from threading import Thread
+from datetime import datetime
+from flask import Flask
+from flask import render_template
 
 
-string = secrets.token_hex(32)
+app = Flask(__name__)
 
-while True:
-    print(string, file=sys.stdout, flush=True)
-    time.sleep(5)
+@app.route("/")
+def index():
+    return string
+
+def main():
+    global string
+
+    while True:
+        string = f"[{datetime.now()}] {secrets.token_hex(32)}"
+
+        print(string, file=sys.stdout, flush=True)
+
+        time.sleep(5)
+
+thread = Thread(target=main)
+thread.start()
